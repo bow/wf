f7579199af5facb9f2f6e6ffade5e54cb5874240
bow <bow@bow.web.id>
2020-01-03 22:03:40 +0700

    style: Update main.go import and struct styles

--- main.go
+++ main.go
@@ -5,36 +5,35 @@ import (
 	"os"
 	"time"
 
-	. "github.com/bow/wait-for/wait"
+	"github.com/bow/wait-for/wait"
 )
 
 func main() {
-
 	// TODO: Make these variables configurable via CLI.
 	isQuiet := false
 	waitTimeout := 3 * time.Second
 	checkFreq := 300 * time.Millisecond
 	statusFreq := 500 * time.Millisecond
 	replyTimeout := 500 * time.Millisecond
-	cfgs := []*TCPInputConfig{
-		&TCPInputConfig{
+	cfgs := []*wait.TCPInputConfig{
+		{
 			Addr:         "localhost:8000",
 			CheckFreq:    checkFreq,
 			ReplyTimeout: replyTimeout,
 		},
-		&TCPInputConfig{
+		{
 			Addr:         "localhost:5672",
 			CheckFreq:    checkFreq,
 			ReplyTimeout: replyTimeout,
 		},
-		&TCPInputConfig{
+		{
 			Addr:         "google.com:80",
 			CheckFreq:    checkFreq,
 			ReplyTimeout: replyTimeout,
 		},
 	}
 
-	msg := WaitAllTCP(cfgs, waitTimeout, statusFreq, isQuiet)
+	msg := wait.WaitAllTCP(cfgs, waitTimeout, statusFreq, isQuiet)
 	if msg.Err != nil {
 		if !isQuiet {
 			fmt.Printf("ERROR: %s\n", msg.Err)
